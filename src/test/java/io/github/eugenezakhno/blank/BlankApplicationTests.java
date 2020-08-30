9c645c6a4e7382496f7091471c3834d59f7b091d
EugeneZakhno <zohan1234567@gmail.com>
2020-08-30 23:05:54 +0300

    first init

---
+++ src/test/java/io/github/eugenezakhno/blank/BlankApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eugenezakhno.blank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
